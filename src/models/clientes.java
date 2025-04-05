db81d3062b36716eaf14c2790fa4a54d60c32dc8
anaelzan. <anaelzanunespedro@gmail.com>
2025-04-05 15:33:34 -0300

    feat: instrutor

--- src/models/clientes.java
+++ src/models/clientes.java
@@ -1,4 +1,20 @@
 package models;
 
-public class clientes {
+abstract class clientes extends Pessoa {
+    private String planoCliente;
+
+    public clientes(String nome, int idade, String planoCliente) {
+        super(nome, idade);
+        this.planoCliente = planoCliente;
+
+    }
+
+    @Override
+    public String getDescricao() {
+        return "clientes{" +
+                "nomePessoa='"+ nomePessoa +'\''+
+                "idadePessoa='"+ idadePessoa +'\''+
+                "planoCliente='" + planoCliente + '\'' +
+                '}';
+    }
 }
