44cd3a9e24c81edb7ad7337fc90aa898fffe5fbb
anaelzan. <anaelzanunespedro@gmail.com>
2025-04-05 14:28:30 -0300

    feat: projeto academia

---
+++ src/models/Pessoa.java
@@ -0,0 +1,24 @@
+package models;
+
+abstract class Pessoa {
+    protected String nomePessoa;
+    protected int idadePessoa;
+
+    public Pessoa(String nome, int idade){
+        this.nomePessoa = nome;
+        this.idadePessoa = idade;
+
+
+    }
+
+    public int getIdade() {
+        return idadePessoa;
+    }
+
+    public String getNome() {
+        return nomePessoa;
+
+    }
+    public abstract String getDescricao ();
+
+}
